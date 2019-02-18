2d277a5ada5aed925f4bdccce276d1318e1afce5
pin4in <sergii.korkh@gmail.com>
2019-02-18 15:45:10 +0200

    add PORT env var

--- config/default.js
+++ config/default.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  // secret data can be moved to env variables
-  // or a separate config
-  port: 3000,
+  port: process.env.PORT || 3000,
   host: '127.0.0.1',
   secret: 'mysecret',
   root: process.cwd(),
